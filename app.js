a6314d98bc3c793b049f8e6dc02284c3fb1bedf7
Dane Christensen <danechristensen92@gmail.com>
2020-02-04 15:55:12 -0800

    api loop through

--- app.js
+++ app.js
@@ -1,46 +1,31 @@
-let latitude = "40.2398"
-    let longitude = "-76.9200"
-
-    function latFunc() {
-        let latitude = document.getElementById("lat").value;
-        console.log(latitude);
-    }
-
-    function longFunc() {
-        let longitude = document.getElementById("long").value;
-        console.log(longitude);
-    }
-
+    // let latitude = "40.2398"
+    // let longitude = "-76.9200"
 
 function callApi() {
-    const latitude = document.getElementById("lat").value;
+    let latitude = document.getElementById("lat").value;
     let longitude = document.getElementById("long").value;
-  console.log("latitude: " + latitude);
-   console.log("longitude: " + longitude);
- var url = "https://www.hikingproject.com/data/get-trails?lat=" + latitude + "&lon=" + longitude + "&maxDistance=10&key=200441732-638bb9d20ee445380f3da709a8c7ebbe";
- console.log(url);
-   
- $.getJSON(url, function (data) {
- console.log(data);
- console.log(data.trails[0].location);
+    console.log("latitude: " + latitude);
+    console.log("longitude: " + longitude);
+    const url = "https://www.hikingproject.com/data/get-trails?lat=" + latitude + "&lon=" + longitude + "&maxDistance=10&key=200441732-638bb9d20ee445380f3da709a8c7ebbe";
+    console.log(url);
 
- let image = data.trails[0].imgSmallMed;
- let name = data.trails[0].name
- let stars = data.trails[0].stars
- let starvotes = data.trails[0].starVotes
- let location = data.trails[0].location
- let length = data.trails[0].length
- let ascent = data.trails[0].ascent
- $('.image').attr('src', image);
- $('.name').append(name);
- $('.stars').append(stars);
- $('.starvotes').append(starvotes);
- $('.location').append(location);
- $('.length').append(length);
- $('.ascent').append(ascent);
- }
-);
- };
+    $.getJSON(url, function (data) {
+        console.log(data);
+        console.log(data.trails[0].location);
 
- 
-
+        $.each(data, function (index, trails) {
+            console.log(trails[2])
+            var i;
+            for (i = 0; i < trails.length; ++i) {
+                let image = data.trails[i].imgSmallMed;
+                let name = data.trails[i].name
+                let stars = data.trails[i].stars
+                let starvotes = data.trails[i].starVotes
+                let location = data.trails[i].location
+                let length = data.trails[i].length
+                let ascent = data.trails[i].ascent
+                $('.trail-container').append('<div class="image"><img src="' + image + '"/></div><p class="name">' + name + '</p><p class="stars">' + stars + '</p><p class="starvotes">' + starvotes + '</p><p class="location">' + location + '</p><p class="length">' + length + '</p><p class="ascent">' + ascent + '</p>');
+            };
+        })
+    });
+}
