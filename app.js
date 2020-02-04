1549fa2e26091e3bf3f705a4739b57101bb9c01b
Dane Christensen <danechristensen92@gmail.com>
2020-02-04 12:40:14 -0800

    added coords functionality

--- app.js
+++ app.js
@@ -1,17 +1,4 @@
-// const button = document.querySelector('button')
-
-// document.getElementById("latitudeInput").value = "enter the latitude of your hiking destination";
-// document.getElementById("longitudeInput").value = "enter the latitude of your hiking destinition";
-
-// function getValue() {
-//     let longitudeInput = document.getElementById('longitudeInput').value;
-//     let latitudeInput = document.getElementById('latitudeInput').value;
-//     latitudeInput == latitude
-//     longitudeInput == longitude
-//     location.reload(true);
-//     }
-
-    let latitude = "40.2398"
+let latitude = "40.2398"
     let longitude = "-76.9200"
 
     function latFunc() {
@@ -24,28 +11,36 @@
         console.log(longitude);
     }
 
-    latFunc();
-    longFunc();
 
-$.getJSON("https://www.hikingproject.com/data/get-trails?lat=" + latitude + "&lon=" + longitude + "&maxDistance=10&key=200441732-638bb9d20ee445380f3da709a8c7ebbe", function (data) {
-    
+function callApi() {
+    const latitude = document.getElementById("lat").value;
+    let longitude = document.getElementById("long").value;
+  console.log("latitude: " + latitude);
+   console.log("longitude: " + longitude);
+ var url = "https://www.hikingproject.com/data/get-trails?lat=" + latitude + "&lon=" + longitude + "&maxDistance=10&key=200441732-638bb9d20ee445380f3da709a8c7ebbe";
+ console.log(url);
+   
+ $.getJSON(url, function (data) {
+ console.log(data);
+ console.log(data.trails[0].location);
 
-    console.log(data.trails[0].location);
-
-    let image = data.trails[0].imgSmallMed;
-    let name = data.trails[0].name
-    let stars = data.trails[0].stars
-    let starvotes = data.trails[0].starVotes
-    let location = data.trails[0].location
-    let length = data.trails[0].length
-    let ascent = data.trails[0].ascent
-    $('.image').attr('src', image);
-    $('.name').append(name);
-    $('.stars').append(stars);
-    $('.starvotes').append(starvotes);
-    $('.location').append(location);
-    $('.length').append(length);
-    $('.ascent').append(ascent);
-    }
+ let image = data.trails[0].imgSmallMed;
+ let name = data.trails[0].name
+ let stars = data.trails[0].stars
+ let starvotes = data.trails[0].starVotes
+ let location = data.trails[0].location
+ let length = data.trails[0].length
+ let ascent = data.trails[0].ascent
+ $('.image').attr('src', image);
+ $('.name').append(name);
+ $('.stars').append(stars);
+ $('.starvotes').append(starvotes);
+ $('.location').append(location);
+ $('.length').append(length);
+ $('.ascent').append(ascent);
+ }
 );
+ };
+
+ 
 
