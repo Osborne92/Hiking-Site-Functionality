bf6c41ef14b33f908dcd3dc451f8a43c0a5fed65
Dane Christensen <danechristensen92@gmail.com>
2020-02-25 11:10:59 -0800

    combined api w/ bootstrap collage

--- app.js
+++ app.js
@@ -17,14 +17,14 @@ function callApi() {
             console.log(trails[2])
             let i;
             for (i = 0; i < trails.length; ++i) {
-                let image = data.trails[i].imgSmallMed;
+                let image = data.trails[i].imgMedium;
                 let name = data.trails[i].name
                 let stars = data.trails[i].stars
                 let starvotes = data.trails[i].starVotes
                 // let location = data.trails[i].location
                 // let length = data.trails[i].length
                 // let ascent = data.trails[i].ascent
-                $('.card-columns').append('<div class="card bg-dark text-white"><img class="card-img" src="' + image + '"/><div class="card-img-overlay"> <p class="name">' + name + '</p><p class="stars">' + stars + '</p><p class="starvotes">' + starvotes + '</p></div></div>    ');
+                $('.card-columns').append('<div class="card bg-dark text-white"><img class="card-img" src="' + image + '"/><div class="card-img-overlay"> <p class="name">' + name + '</p><p class="stars">' + stars + " stars" + '</p><p class="starvotes">' + starvotes + " votes" + '</p></div></div>    ');
                 // $('.card-img-overlay').append('<p class="card-title">' + name + '</p><p class="card-text">' + stars + '</p><p class="card-text">' + starvotes + '</p>');
                 // $('.trail-container').append('<div class="image"><img src="' + image + '"/></div><p class="name">' + name + '</p><p class="stars">' + stars + '</p><p class="starvotes">' + starvotes + '</p><p class="location">' + location + '</p><p class="length">' + length + '</p><p class="ascent">' + ascent + '</p>');
             };
