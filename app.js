e10165b8002df35d2ab1679ccbde13c62364c8aa
Dane Christensen <danechristensen92@gmail.com>
2020-02-25 09:57:20 -0800

    image sizing

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ function callApi() {
                 // let location = data.trails[i].location
                 // let length = data.trails[i].length
                 // let ascent = data.trails[i].ascent
-                $('.trail-container').append('<div class="trail-card"> <div class="image"><img src="' + image + '"/></div><div class="trail-information"> <p class="name">' + name + '</p><p class="stars">' + stars + '</p><p class="starvotes">' + starvotes + '</p></div></div>    ');
+                $('.card-columns').append('<div class="card bg-dark text-white"> <div class="image"><img src="' + image + '"/></div><div class="card-img-overlay"> <p class="name">' + name + '</p><p class="stars">' + stars + '</p><p class="starvotes">' + starvotes + '</p></div></div>    ');
                 // $('.card-img-overlay').append('<p class="card-title">' + name + '</p><p class="card-text">' + stars + '</p><p class="card-text">' + starvotes + '</p>');
                 // $('.trail-container').append('<div class="image"><img src="' + image + '"/></div><p class="name">' + name + '</p><p class="stars">' + stars + '</p><p class="starvotes">' + starvotes + '</p><p class="location">' + location + '</p><p class="length">' + length + '</p><p class="ascent">' + ascent + '</p>');
             };
